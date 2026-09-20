2418eba887546b5e877dcd82bf6877040a52b343
9pwm6nfm5b-glitch <9pwm6nfm5b@privaterelay.appleid.com>
2026-09-21 01:43:31 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,1 +1,14 @@
-const CACHE='bunexa-pwa-v1';const APP=['/','/index.html','/manifest.json','/icons/bunexa-icon.svg'];self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(APP)));self.skipWaiting()});self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==CACHE).map(x=>caches.delete(x))));self.clients.claim()});self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(c=>c||fetch(e.request).then(r=>{caches.open(CACHE).then(x=>x.put(e.request,r.clone()));return r}).catch(()=>caches.match('/index.html'))))});
+const CACHE = 'bywise-v22';
+const CORE = ['./', './index.html', './manifest.json', './icons/bywise-icon.svg'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(r => {
+    const copy = r.clone(); caches.open(CACHE).then(c => c.put(e.request, copy)); return r;
+  }).catch(() => caches.match('./index.html'))));
+});
